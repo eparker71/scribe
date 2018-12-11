896d76dea8e2594bad9e329b6e22c4d3dfb74999
epp785 <eric_parker@harvard.edu>
2018-12-10 21:27:28 -0500

    test web hook

--- index.php
+++ index.php
@@ -40,14 +40,15 @@
         </article>
       </section>
     </main>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript" src="events.js"></script>
-    <script type="text/javascript" src="nav.js"></script>
+
     <footer>
       <?php include 'login.php';?>
       <?php include 'footer.php';?>
     </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="events.js"></script>
+    <script type="text/javascript" src="nav.js"></script>
   </body>
 
 </html>
